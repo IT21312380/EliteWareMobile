e904f1d10daa206c264485366f2b15375fffa874
hirudaka <hirudakakumarasingha@gmail.com>
2024-10-01 12:21:45 +0530

    review init

---
+++ app/src/main/java/com/example/elitewear_mobile/Review.kt
@@ -0,0 +1,39 @@
+package com.example.elitewear_mobile
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.elitewear_mobile.Adapters.ProductAdapter
+import com.example.elitewear_mobile.Adapters.ReviewAdapter
+import com.example.elitewear_mobile.Network.ApiClient
+import com.example.elitewear_mobile.Network.ApiClient2
+
+class Review : AppCompatActivity() {
+
+    private lateinit var reviewListView : ListView
+    private lateinit var reviewAdapter: ReviewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_review)
+
+        reviewListView = findViewById(R.id.reviewListView)
+
+        ApiClient2.fetchReviews { reviews ->
+            runOnUiThread {
+                reviewAdapter = ReviewAdapter(this, reviews)
+                reviewListView.adapter = reviewAdapter
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
