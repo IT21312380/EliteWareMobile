362fbc423cb6d7f587f1ee53ec4bb2e8c0ddfc91
hirudaka <hirudakakumarasingha@gmail.com>
2024-10-08 16:51:59 +0530

    UI changed #3

--- app/src/main/java/com/example/elitewear_mobile/UserUpdateActivity.kt
+++ app/src/main/java/com/example/elitewear_mobile/UserUpdateActivity.kt
@@ -1,11 +1,13 @@
 package com.example.elitewear_mobile
 
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -60,6 +62,34 @@ class UserUpdateActivity : AppCompatActivity() {
        //  Set click listener for the save button
         saveButton.setOnClickListener {
             updateUserProfile(email)
+        }
+
+        val HomeButton = findViewById<ImageView>(R.id.navHomeUnClick)
+        val ProfilePageButton = findViewById<ImageView>(R.id.navProfileUnClick)
+        val CartPageButton = findViewById<ImageView>(R.id.navCartUnClick)
+        val NotifyPageButton = findViewById<ImageView>(R.id.navNotifyUnClick)
+        val OrderHistoryButton = findViewById<ImageView>(R.id.navOrderHistoryUnClick)
+
+        OrderHistoryButton.setOnClickListener {
+            val intent = Intent(this, OrdersActivity::class.java)
+            startActivity(intent)
+        }
+
+        HomeButton.setOnClickListener {
+            val intent = Intent(this, ProductListActivity::class.java)
+            startActivity(intent)
+        }
+        ProfilePageButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        CartPageButton.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
+        NotifyPageButton.setOnClickListener {
+            val intent = Intent(this, NotificationsActivity::class.java)
+            startActivity(intent)
         }
     }
 
